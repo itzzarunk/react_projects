57489e73e7cedb9d8a38e9558c08545b0c3a8b7b
itzzarunk <aj.arunkumar1@gmail.com>
2025-05-15 19:26:27 +0530

    Add files via upload

--- Header.js
+++ Header.js
@@ -2,29 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const isLoggedIn = localStorage.getItem('token');
-  
-  return (
-    <nav className="header">
-      <Link to="/" className="logo">BloodDonor</Link>
-      <div className="nav-links">
-        <Link to="/donors">Find Donors</Link>
-        <Link to="/requests">Requests</Link>
-        <Link to="/donate">Donate</Link>
-        {isLoggedIn ? (
-          <button onClick={() => {
-            localStorage.removeItem('token');
-            window.location = '/login';
-          }}>Logout</button>
-        ) : (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
-        )}
-      </div>
-    </nav>
-  );
+    const isLoggedIn = localStorage.getItem('token');
+
+    return (
+        <nav className="header">
+            <Link to="/" className="logo">BloodDonor</Link>
+            <div className="nav-links">
+                <Link to="/donors">Find Donors</Link>
+                <Link to="/requests">Requests</Link>
+                <Link to="/donate">Donate</Link>
+                {isLoggedIn ? (
+                    <button onClick={() => {
+                        localStorage.removeItem('token');
+                        window.location = '/login';
+                    }}>Logout</button>
+                ) : (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
+            </div>
+        </nav>
+    );
 };
 
 export default Header;
